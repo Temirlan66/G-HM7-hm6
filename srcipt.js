1608e7cedcfa2a586240302a8f35484c4e107d33
Temirlan <admin@Temirlans-MacBook-Pro.local>
2023-05-02 00:30:42 +0600

    hm7

--- srcipt.js
+++ srcipt.js
@@ -2,53 +2,61 @@ const block = document.querySelector(".block");
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
 
+// Извиняюсь что нарушил  еще принцип DRY , другого варианта не нашел....(
+
 let count = 1;
-const url = `https://jsonplaceholder.typicode.com/todos/1`;
+const request = async () => {
+  try {
+    const reponce = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${count}`
+    );
+    const data = await reponce.json();
+    block.innerHTML = `
+          <h1>Count:${count}</h1>
+            <h2>${data.title}</h2>
+           <span>${data.id}</span>
+            <h3>${data.completed}</h3>`;
 
-btnNext.onclick = () => {
-  count++;
-  fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-    .then((reponce) => reponce.json())
-    .then((data) => {
+    btnNext.addEventListener("click", async () => {
+      count++;
+      const reponce = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${count}`
+      );
+      const data = await reponce.json();
       block.innerHTML = `
-      <h1>Count:${count}</h1>
-        <h2>${data.title}</h2>
-        <span>${data.id}</span>
-        <h3>${data.completed}</h3>
-        `;
+            <h1>Count:${count}</h1>
+              <h2>${data.title}</h2>
+             <span>${data.id}</span>
+              <h3>${data.completed}</h3>`;
     });
+
+    btnPrev.addEventListener("click", async () => {
+      count = 1;
+      const reponce = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${count}`
+      );
+      const data = await reponce.json();
+      block.innerHTML = `
+            <h1>Count:${count}</h1>
+              <h2>${data.title}</h2>
+             <span>${data.id}</span>
+              <h3>${data.completed}</h3>`;
+    });
+    console.log(count);
+  } catch (error) {
+    console.log("ERROR");
+  }
+};
+request();
+
+const requestPosts = async () => {
+  try {
+    const reponce = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await reponce.json();
+    console.log("POSTS", data);
+  } catch (error) {
+    console.log("ERROR");
+  }
 };
 
-btnPrev.onclick = () => {
-  count =1
-  fetch(url)
-    .then((reponce) => reponce.json())
-    .then((data) => {
-      block.innerHTML = `
-      <h1>Count:${count}</h1>
-      <h2>${data.title}</h2>
-      <span>${data.id}</span>
-      <h3>${data.completed}</h3>
-      `;
-    });
-};
-
-const getRequest = () => {
-  fetch(url)
-    .then((reponce) => reponce.json())
-    .then((data) => {
-      block.innerHTML = `
-        <h2>${data.title}</h2>
-        <span>${data.id}</span>
-        <h3>${data.completed}</h3>
-        `;
-    });
-};
-getRequest();
-
-
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((reponce) => reponce.json())
-  .then((data) => {
-    console.log(data);
-  });
+requestPosts();
